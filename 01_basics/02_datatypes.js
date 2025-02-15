7b33bd986dd9223fb41417ecead7dc00b54e9dc3
Atul-singh707 <atul20526@gmail.com>
2025-02-15 18:58:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code in newer version
+
+//alert( 3 + 3) // we are using nodeJS, not browser
+
+console.log(3 + 3) 
+
+//code relatbility should be high
+
+console.log("Atul")
+
+let name = "Atul"
+let age = 21 
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (its also defined as an object)
+// undefined =>
+// symbol => unique
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
