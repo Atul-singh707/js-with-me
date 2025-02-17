c277cb9b0cb18a7bbadabade929c30212a908929
Atul-singh707 <atul20526@gmail.com>
2025-02-17 19:23:08 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -23,6 +23,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************Opration*************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+
+// let str1 = "hello"
+// let str2 = " Atul"
+
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
 
