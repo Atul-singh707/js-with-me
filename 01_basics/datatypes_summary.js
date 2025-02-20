d6dac6708b14b2fe3b61db968a8b92a34a7ae93e
Atul-singh707 <atul20526@gmail.com>
2025-02-20 18:17:39 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+// Primitive datatype 
+
+// 7 types : String, Numbers, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.34
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); 
+
+// const bigNumbers = 68736487617264812634n
+
+// Reference(non-primitive) datatype
+
+// Array, Objects, functions 
+// const heroes [ "shaktiman" , "nagraj" , "doga"];
+ let myObj = {
+        name: "Atul",
+        age: "21",
+   }
+
+const myFunction = function () {
+    console.log("Hello world");
+    
+}
