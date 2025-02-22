5ff8205193bef1ea163c29778c09b82fc3e15c7a
Atul-singh707 <atul20526@gmail.com>
2025-02-22 19:07:17 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "atul"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+//string interpolesion//
+
+const gameName = new String ('Atul001')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
