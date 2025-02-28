921e4be3211b56b11f3f1b89d7174774c6b38816
Atul-singh707 <atul20526@gmail.com>
2025-02-28 19:29:57 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+//arrays//
+
+const myArr = [0, 1, 2, 3, 4, 5] 
+const myHeros = [ "shaktiman", "nagraaj"] 
+
+const myArr2 = new Array(1, 2, 3, 4,)
+console.log(myArr[1]);
+
+//Array Methods//
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+
+// const newArray = myArr.join()
+
+// console.log(myArr);
+// console.log(newArray);
+
+//slice or splice//
