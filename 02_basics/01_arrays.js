d9054e1066a8ce4c69e1add4d15c194ad52fc4dd
Atul-singh707 <atul20526@gmail.com>
2025-03-03 18:13:14 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = [ "shaktiman", "nagraaj"] 
 
 const myArr2 = new Array(1, 2, 3, 4,)
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 //Array Methods//
 
@@ -23,3 +23,15 @@ console.log(myArr[1]);
 // console.log(newArray);
 
 //slice or splice//
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
