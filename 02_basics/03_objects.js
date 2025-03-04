64c70ec008537585a1a591f7f5bfed993dd342c6
Atul-singh707 <atul20526@gmail.com>
2025-03-04 18:10:36 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//singleton
+
+//object literals
+
+const jsUser = {
+    name: "Atul",
+    "full Name": "Atul Singh",
+    age: 21,
+    location: "faridabad",
+    email: "atul@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(jsUser.email);
+console.log(jsUser["full Name"]);
+
