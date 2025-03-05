30acc8cb833b3ac00d94447e5df0a55c2ef047d2
Atul-singh707 <atul20526@gmail.com>
2025-03-05 17:47:14 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,16 +1,31 @@
 //singleton
 
 //object literals
+const mySym = Symbol("key1")
 
 const jsUser = {
     name: "Atul",
     "full Name": "Atul Singh",
+    [mySym]: "mykey1",
     age: 21,
     location: "faridabad",
     email: "atul@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-console.log(jsUser.email);
-console.log(jsUser["full Name"]);
+// console.log(jsUser.email);
+// console.log(jsUser["full Name"]);
+// console.log(typeof jsUser[mySym]);
 
+
+jsUser.email = "atul@chatgpt.com"
+// Object.freeze(jsUser)
+jsUser.email = "atul@microsoft.com"
+// console.log(jsUser);
+
+
+jsUser.greetings = function(){
+    console.log("hello JS user");
+    
+}
+console.log(jsUser.greetings);
