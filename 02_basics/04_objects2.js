4949e572631a2fc12029f598c23d74ccf452d9b6
Atul-singh707 <atul20526@gmail.com>
2025-03-08 17:44:37 +0000

    jason talk

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -47,10 +47,28 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    courseName: "JavaScript",
+    price: "999",
+    courseInstructor: "Atul"
+}
+
+const {courseInstructor: Instructor} = course
+
+console.log(Instructor);
+
+// {
+//     "name": "Atul",
+//     "courseName": "JavaScript",
+//     "price": "free"
+
+// }
