2ea081867c485af0742539904420229ca2a77f39
Atul-singh707 <atul20526@gmail.com>
2025-03-10 18:45:15 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,34 @@
+
+function sayMyName (){
+    console.log("A");
+    console.log("t");
+    console.log("u");
+    console.log("l");
+}
+// sayMyName()
+
+// function addTwoNumbers (number1, number2){
+//          console.log(number1 + number2);
+         
+// }
+// addTwoNumbers(3, 6)
+
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2
+    return result
+    
+}
+const result = addTwoNumbers(3, 6)
+// console.log("result:",  result);
+
+
+function loginUserMassage(username){
+    if (username === undefined) {
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMassage("Atul"));
+console.log(loginUserMassage());
+
