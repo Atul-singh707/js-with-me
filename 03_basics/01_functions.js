d63356aeb2ec5f123979fb630d12fe231c5a5d2e
Atul-singh707 <atul20526@gmail.com>
2025-03-12 17:39:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,10 +1,10 @@
 
-function sayMyName (){
-    console.log("A");
-    console.log("t");
-    console.log("u");
-    console.log("l");
-}
+// function sayMyName (){
+//     console.log("A");
+//     console.log("t");
+//     console.log("u");
+//     console.log("l");
+// }
 // sayMyName()
 
 // function addTwoNumbers (number1, number2){
@@ -13,28 +13,36 @@ function sayMyName (){
 // }
 // addTwoNumbers(3, 6)
 
-function addTwoNumbers (number1, number2){
-    let result = number1 + number2
-    return result
+// function addTwoNumbers (number1, number2){
+//     let result = number1 + number2
+//     return result
     
-}
-const result = addTwoNumbers(3, 6)
+// }
+// const result = addTwoNumbers(3, 6)
 // console.log("result:",  result);
 
 
-function loginUserMassage(username){
-    if (username === undefined) {
-        console.log("Please enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMassage(username){
+//     if (username === undefined) {
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 // console.log(loginUserMassage("Atul"));
 // console.log(loginUserMassage());
 
-function calculatCartPrice(...num1){
-    return num1 
+// function calculatCartPrice(...num1){
+//     return num1 
+// }
+// console.log(calculatCartPrice(200, 300, 400));
+
+const user = {
+    username: "Atul",
+    price: 199
 }
-console.log(calculatCartPrice(200, 300, 400));
-
-
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
